0368f0d84e5320415724055509909bacf8e74151
J.R. Titko <j.r.titko.professional@gmail.com>
2019-06-22 13:14:53 -0500

    TEST-RED - DataPortal should throw an InvalidSourceException if unknown datasource

---
+++ src/test/groovy/com/industriousgnomes/tddrefactorkata/service/DataPortalTest.groovy
@@ -0,0 +1,34 @@
+package com.industriousgnomes.tddrefactorkata.service
+
+import com.industriousgnomes.tddrefactorkata.cassandra.CassandraConnector
+import com.industriousgnomes.tddrefactorkata.mongo.MongoConnector
+import spock.lang.Specification
+import spock.lang.Subject
+
+class DataPortalTest extends Specification {
+
+    @Subject
+    DataPortal dataPortal
+
+    MongoConnector mongoConnector = Mock()
+
+    CassandraConnector cassandraConnector = Mock()
+
+    void setup() {
+        dataPortal = new DataPortal(
+                mongoConnector: mongoConnector,
+                cassandraConnector: cassandraConnector
+        )
+    }
+
+    def "Should throw an InvalidSourceException if unknown datasource"() {
+        given:
+            System.setProperty("cassandra.column_table", "unknown");
+
+        when:
+            dataPortal.copyDataOver()
+
+        then:
+            thrown InvalidSourceException
+    }
+}
