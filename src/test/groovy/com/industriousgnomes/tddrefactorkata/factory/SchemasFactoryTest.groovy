c226b0361acff2542c4bdb9d776039ee8ba941f0
J.R. Titko <j.r.titko.professional@gmail.com>
2019-06-22 13:50:12 -0500

    TEST - SchemaFactory should throw an InvalidSourceException if unknown datasource

--- src/test/groovy/com/industriousgnomes/tddrefactorkata/factory/SchemasFactoryTest.groovy
+++ src/test/groovy/com/industriousgnomes/tddrefactorkata/factory/SchemasFactoryTest.groovy
@@ -1,23 +1,32 @@
 package com.industriousgnomes.tddrefactorkata.factory
 
 
+import com.industriousgnomes.tddrefactorkata.cassandra.CassandraConnector
 import com.industriousgnomes.tddrefactorkata.exceptions.InvalidSourceException
-import spock.lang.Ignore
 import spock.lang.Specification
+import spock.lang.Subject
 
 class SchemasFactoryTest extends Specification {
 
-    @Ignore("Code moved to SchemaFactory")
+    @Subject
+    SchemasFactory schemasFactory
+
+    CassandraConnector cassandraConnector = Mock()
+
+    void setup() {
+        schemasFactory = new SchemasFactory(
+                cassandraConnector: cassandraConnector
+        )
+    }
+
     def "Should throw an InvalidSourceException if unknown datasource"() {
         given:
             System.setProperty("datasource.name", "unknown");
 
         when:
-            dataPortal.copyDataOver()
+            schemasFactory.getSchemas()
 
         then:
             thrown InvalidSourceException
     }
-
-
 }
