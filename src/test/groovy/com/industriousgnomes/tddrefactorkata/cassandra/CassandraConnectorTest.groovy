51ee0be414dba037eaeec4aedb30ab48b2b79ae6
J.R. Titko <j.r.titko.professional@gmail.com>
2019-06-22 13:07:18 -0500

    TEST-RED - CassandraConnector should connect and close the connection to Cassandra

---
+++ src/test/groovy/com/industriousgnomes/tddrefactorkata/cassandra/CassandraConnectorTest.groovy
@@ -0,0 +1,40 @@
+package com.industriousgnomes.tddrefactorkata.cassandra
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class CassandraConnectorTest extends Specification {
+
+    @Subject
+    CassandraConnector cassandraConnector
+
+    void setup() {
+        System.setProperty("cassandra.cluster", "localhost");
+        System.setProperty("cassandra.port", "9042");
+        System.setProperty("cassandra.user", "someuser");
+        System.setProperty("cassandra.password", "somepassword");
+
+        cassandraConnector = new CassandraConnector()
+    }
+
+    def "Should connect and close the connection to Cassandra"() {
+        given:
+            //
+
+        when:
+            cassandraConnector.connect()
+
+        then:
+            noExceptionThrown()
+            !cassandraConnector.getSession().closed
+            !cassandraConnector.cluster.closed
+
+        when:
+            cassandraConnector.close()
+
+        then:
+            noExceptionThrown()
+            cassandraConnector.getSession().closed
+            cassandraConnector.cluster.closed
+    }
+}
